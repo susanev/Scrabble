79e36499492370a395451cf0fc76dbfc51f3fda9
susan <susan.ra.evans@gmail.com>
2016-08-31 22:55:55 -0700

    added tests for highest_score_from

--- lib/scrabble_test.rb
+++ lib/scrabble_test.rb
@@ -4,5 +4,18 @@ require 'minitest/pride'
 require_relative 'Scrabble'
 
 class ScrabbleTest < Minitest::Test
+	def test_self_score
+		assert_equal 9, Scrabble::Scoring.score("apple")
+		assert_equal 0, Scrabble::Scoring.score("")
+		assert_equal 66, Scrabble::Scoring.score("zebadia")
+	end
 
+	def test_self_highest_score_from
+		assert_equal "a", Scrabble::Scoring.highest_score_from(["a"])
+		assert_equal "ab", Scrabble::Scoring.highest_score_from(["a", "ab"])
+		assert_equal "ab", Scrabble::Scoring.highest_score_from(["ab", "ec"])
+		assert_equal "zzzzzzz", Scrabble::Scoring.highest_score_from(["ab", "ec", "zzzzzzz"])
+		assert_equal "zzzzzzz", Scrabble::Scoring.highest_score_from(["aaaaaaa", "ec", "zzzzzzz"])
+		assert_equal "z", Scrabble::Scoring.highest_score_from(["z", "ja", "bba"])
+	end
 end
