3660ce9c14bff2130bdeb3e2a96a327857f81958
susan <susan.ra.evans@gmail.com>
2016-08-31 23:27:57 -0700

    added tests for player

--- lib/scrabble_test.rb
+++ lib/scrabble_test.rb
@@ -18,4 +18,43 @@ class ScrabbleTest < Minitest::Test
 		assert_equal "zzzzzzz", Scrabble::Scoring.highest_score_from(["aaaaaaa", "ec", "zzzzzzz"])
 		assert_equal "z", Scrabble::Scoring.highest_score_from(["z", "ja", "bba"])
 	end
+
+	def test_player_init
+		p = Scrabble::Player.new("susan")
+		assert_equal "susan", p.name
+		assert_equal [], p.plays
+		assert_equal 0, p.total_score
+	end
+
+	def test_player_play
+		p = Scrabble::Player.new("susan")
+		p.play("apple")
+		assert_equal ["apple"], p.plays
+		assert_equal 9, p.total_score
+	end
+
+	def test_player_won?
+		p = Scrabble::Player.new("susan")
+		assert_equal false, p.won?
+		p.play("apple")
+		p.play("zzzzzzz")
+		assert_equal 108, p.total_score
+		assert_equal true, p.won?
+		assert_equal false, p.play("apple")
+		assert_equal 108, p.total_score
+	end
+
+	def test_player_highest_scoring_word
+		p = Scrabble::Player.new("susan")
+		p.play("apple")
+		p.play("zzzzzzz")
+		assert_equal "zzzzzzz", p.highest_scoring_word
+	end
+
+	def test_player_highest_word_score
+		p = Scrabble::Player.new("susan")
+		p.play("apple")
+		p.play("zzzzzzz")
+		assert_equal 99, p.highest_word_score
+	end
 end
