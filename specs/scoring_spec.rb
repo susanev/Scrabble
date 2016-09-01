ea761d4287e8a9aebb83dd0eb6d04b5783c4b022
susan <susan.ra.evans@gmail.com>
2016-09-01 10:46:44 -0700

    added more specs for the scoring methods

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -7,6 +7,46 @@ describe 'Testing scoring_setup_score_chart' do
 	end
 end
 
+describe 'Testing scoring_score' do
+	it 'must score the empty string as 0' do
+		expect(Scrabble::Scoring.score("").must_equal(0))
+	end
+
+	it 'must score a basic word correctly' do
+		expect(Scrabble::Scoring.score("apple").must_equal(9))
+	end
+
+	it 'must add 50 to a word with 7 letters' do
+		expect(Scrabble::Scoring.score("zebadia").must_equal(66))
+	end
+end
+
+describe 'Testing scoring_highest_score_from' do
+	it 'must return the item from an array of length 1' do
+		expect(Scrabble::Scoring.highest_score_from(["a"]).must_equal("a"))
+	end
+
+	it 'must return the highest scoring word from a pair' do
+		expect(Scrabble::Scoring.highest_score_from(["a", "ab"]).must_equal("ab"))
+	end
+
+	it 'must return the first word in list if tie in score and length' do
+		expect(Scrabble::Scoring.highest_score_from(["ab", "ec"]).must_equal("ab"))
+	end
+
+	it 'must return seven letter word has highest score' do
+		expect(Scrabble::Scoring.highest_score_from(["ab", "ec", "zzzzzzz"]).must_equal("zzzzzzz"))
+	end
+
+	it 'must return highest score, if 2 words of 7 letters' do
+		expect(Scrabble::Scoring.highest_score_from(["aaaaaaa", "ec", "zzzzzzz"]).must_equal("zzzzzzz"))
+	end
+
+	it 'must return shortest length word if tie on scores' do
+		expect(Scrabble::Scoring.highest_score_from(["z", "ja", "bba"]).must_equal("z"))
+	end
+end
+
 # reference
 # describe 'Testing scrabble_score' do
 #   it 'It must raise an IllegalArgument if given a non-String' do
@@ -15,9 +55,5 @@ end
 
 #   it 'Must return greater than 50 when word is equal to 7 tiles' do
 #     expect (Scrabble::Scoring.score("sevenwd")).must_be :>=, 50
-#   end
-
-#   it 'Must return score of 8 if "word".' do
-#     expect (Scrabble::Scoring.score("word")).must_equal(8)
 #   end
 # end
